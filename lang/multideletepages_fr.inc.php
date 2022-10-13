a703c4717dde9aa3a1726cc41e0b9954e38888c1
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-10-13 11:55:30 +0200

    refactor(multideletepage): create i18n files

---
+++ lang/multideletepages_fr.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the YesWiki Extension multideletepages.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+   // templates/multideletedpages.twig
+   'MULTIDELETEPAGES_REFRESH_LIST' => 'Rafraichir la liste',
+   'MULTIDELETEPAGES_CUSTOM_SELECT' => 'Sélection custom',
+   'MULTIDELETEPAGES_TITLE' => 'Gestion des pages en trop',
+   'MULTIDELETEPAGES_NAME' => 'Nom',
+   'MULTIDELETEPAGES_DATE' => 'Date',
+   'MULTIDELETEPAGES_OWNER' => 'Propriétaire',
+   'MULTIDELETEPAGES_USER' => 'Utilisateur',
+   'MULTIDELETEPAGES_DELETE_SELECTION' => 'Supprimer la sélection',
+   'MULTIDELETEPAGES_DELETE_SELECTION_SHORT' => 'Suppr. select.',
+   'MULTIDELETEPAGES_DELETE' => 'Supprimer',
+];
